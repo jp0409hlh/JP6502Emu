454470e492d15fb6709aea5ef1af74018ab6baa2
jp0409hlh <minejingpan0409@gmail.com>
2026-09-04 21:32:18 +0800

    Testing imgui mouse capture (Half working)

--- App/Source/ImguiWindows/VideoWindow.cpp
+++ App/Source/ImguiWindows/VideoWindow.cpp
@@ -27,15 +27,26 @@ void VideoWindow::createSDLTexture(SDL_Renderer* sdl_renderer){
 void VideoWindow::renderImguiWindow(Core::Application& app){
     ImGui::Begin(this->ImguiWindowName.c_str());
     ImTextureID texture_id = (ImTextureID)(intptr_t)(this->TMS9918_texture);
-    ImVec2 size = ImVec2((float)(TMS9918_PIXELS_X), (float)(TMS9918_PIXELS_Y));
+    ImVec2 size = ImVec2((float)(TMS9918_PIXELS_X)*3.2f, (float)(TMS9918_PIXELS_Y)*3.2f);
 
     SDL_SetRenderTarget(app.appGetSDLRenderer(), this->TMS9918_texture);
     SDL_SetRenderDrawColor(app.appGetSDLRenderer(), 255, 0, 0, 255);
-    SDL_FRect rect = { 10, 10, 50, 50 };
-    SDL_RenderFillRect(app.appGetSDLRenderer(), &rect); 
+
+    ImVec2 cur_window_pos = ImGui::GetWindowPos();
+    ImVec2 mouse_pos = ImGui::GetMousePos();
+    ImVec2 relative_pos = ImVec2(mouse_pos.x - cur_window_pos.x, mouse_pos.y - cur_window_pos.y);
+
+    if(ImGui::IsMousePosValid() && ImGui::GetIO().WantCaptureMouse){
+        float rect_x = relative_pos.x;
+        float rect_y = relative_pos.y;
+
+        SDL_FRect rect = { rect_x, rect_y, 50, 50 };
+        SDL_RenderFillRect(app.appGetSDLRenderer(), &rect); 
+
+        SDL_RenderClear(app.appGetSDLRenderer());
+    }
 
     ImGui::Image(texture_id, size);
-
     SDL_SetRenderTarget(app.appGetSDLRenderer(), NULL);
 
     ImGui::End();
